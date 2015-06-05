27283aef28ad75a131d5ab0779cf6ed382d232c7
Marcin Kaczmarek <mkacz91@gmail.com>
2015-06-05 15:12:29 +0200

    32 bit?

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,9 @@
 #include "prefix.hpp"
 
 int main (int argc, char** argv) {
+    cout << "Word size: " << sizeof(word) << "\n"
+         << "WORD_LENGTH: " << WORD_LENGTH << endl;
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
